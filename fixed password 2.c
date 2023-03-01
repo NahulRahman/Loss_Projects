d11d340e052f01836c7dae75ad00aba11f4e0408
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-02 00:51:28 +0600

    Add files via upload

---
+++ fixed password 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    long long int a;
+    scanf("%d",&a);
+    while(1){
+        if(a!=1999) printf("Wrong");
+        else printf("Correct");
+        break;
+    }return 0;
+}
