c74555c9bd3ce25e52bf1652b436633a64f06b4b
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-05 02:10:28 +0600

    Add files via upload

---
+++ V. Comparison.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d%c%d",&a,&c,&b);
+    if(c=='>'){
+        if(a>b) printf("Right\n");
+        else printf("Wrong\n");
+    }if(c=='<'){
+        if(a<b) printf("Right\n");
+        else printf("Wrong\n");
+    }if(c=='='){
+        if(a==b) printf("Right\n");
+        else printf("Wrong\n");
+    }return 0;
+}
